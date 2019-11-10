673da5be7e359652fa184d60476083a88dd1ab90
dhtium <dhtium@163.com>
2019-11-11 00:53:03 +0800

    add springMVC

--- user/src/main/java/org/element/mirror/user/action/UserAction.java
+++ user/src/main/java/org/element/mirror/user/action/UserAction.java
@@ -1,7 +1,32 @@
 package org.element.mirror.user.action;
 
+import org.element.mirror.user.entity.User;
+import org.element.mirror.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  * @author dhtium
  */
+
+@Controller
+@RequestMapping("user")
 public class UserAction {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("get")
+    public User getUser(@RequestParam("id") Integer id) {
+        User user = userService.getUser(id);
+        System.out.println("查询到：" + user);
+        return user;
+    }
+
 }
