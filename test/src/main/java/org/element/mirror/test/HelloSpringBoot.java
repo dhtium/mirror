acb5606dc51389a3c91de10107f172d0f864256c
dhtium <dhtium@163.com>
2019-11-13 11:22:28 +0800

    update ignore file

---
+++ test/src/main/java/org/element/mirror/test/HelloSpringBoot.java
@@ -0,0 +1,21 @@
+package org.element.mirror.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloSpringBoot {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello spring boot !!!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringBoot.class, args);
+    }
+
+}
